974526d3673ec6330132f16f61ea49c56b2317e1
LilMartin <martin_du@qq.com>
2023-08-16 17:51:30 +0800

    build: 新增 dts 打包工具

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 // vite.config.js
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 export default defineConfig({
+  plugins: [dts()],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/mduash.ts'),
       name: 'mduash', // the proper extensions will be added
       fileName: 'mduash',
     },
